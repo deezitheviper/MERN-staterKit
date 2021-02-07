9f45c0839e7500f59776d549d30f20c5865d0206
Epic david <toonerdrill@gmail.com>
2021-02-07 16:12:45 +0100

    register for backend

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,4 +1,72 @@
+const User = require('./models/authmodels.js')
+const expressJwt = require('express-jwt')
+const loadash = require('loadash')
+const { OAuth2Client } = require('google-auth-library')
+const axios = require('axios')
+const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+
+//custom error handler to get error from database
+const { errorHandler } = require('./helpers/dbErrorHandling')
+
+const nodemailer = require('nodemailer')
+
+var transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+    }
+});
+
 exports.registerController = (req, res) => {
     const { name, email, password } = req.body
-    console.log(name, email, password)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const firstError = errors.array().map(error => error.msg)[0]
+        return res.status(422).json({
+            error: firstError
+        })
+    }
+    else {
+        User.findOne({
+            email
+        }).exec((err, user) => {
+            if (user) {
+                return res.status(400).json({
+                    error: "Email already exist"
+                })
+            }
+        })
+        const token = jwt.sign({
+            name,
+            email,
+            password
+        },
+            process.env.JWT_ACTIVATOR,
+            {
+                expiresIn: "15m"
+            })
+    }
+    const emailData = {
+        from: process.env.EMAIL_FROM,
+        to: to,
+        subject: 'Email Confirmation Link',
+        html: `
+        <h1>Click on the Link below to activate</h1>
+        <p>${proccess.env.CLIENT_URL}/user/activate/${token}</p>
+        <hr/>
+        <p>${process.env.CLIENT_URL}</p>
+        `
+    }
+    transporter.sendMail(emailData).then(sent => {
+        return res.json({
+            message: `Confirmation Email sent to ${email}`
+        }).catch(err => {
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        })
+    })
 }
